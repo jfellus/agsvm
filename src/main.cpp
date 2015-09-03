b4bd4f3e05d076074a31914a92d084dab71778a0
Jerome Fellus <jfellus@gmail.com>
2015-09-03 10:40:55 +0200

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -595,9 +595,8 @@ string fE, fEstddev;
 std::ofstream ffE, ffEstddev;
 
 void dump_classifier() {
-	static int i=0;
 	shell(TOSTRING("mkdir -p data" << PREFIX << "w/" << ALGO << "/N" << N << "/l" << LEARNING_RATE << "/"));
-	node[0].w.write(TOSTRING("data" << PREFIX << "w/" << ALGO << "/N" << N << "/l" << LEARNING_RATE << "/" << fmt("%05d",i++) << ".fvec").c_str());
+	node[0].w.write(TOSTRING("data" << PREFIX << "w/" << ALGO << "/N" << N << "/l" << LEARNING_RATE << "/" << fmt("%05f",((float)nbgradients_evaluated/::N)) << ".fvec").c_str());
 }
 
 void compute_errors() {
